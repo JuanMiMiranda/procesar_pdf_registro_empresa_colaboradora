52c29667de2edfaad0b011fad4abd99188f49644
JuanMiMiranda <juanmiranda_90@hotmail.com>
2024-08-27 09:46:59 +0200

    test

--- src/test_script.py
+++ src/test_script.py
@@ -7,12 +7,10 @@ def funcion_escribirLog(mensaje):
     print(mensaje)  # Aquí podrías escribir a un archivo o solo imprimir en consola
 
 # Ruta al archivo de base de datos
-# db_path = 'ruta/a/tu/basededatos.accdb'  # Cambia esto por la ruta real del archivo de base de datos
-db_path = r'C:\Users\JuanMi\Desktop\BD_UNICO_DATOS.accdb'  # Cambia esto por la ruta real del archivo de base de datos
+db_path = 'ruta/a/tu/basededatos.accdb'  # Cambia esto por la ruta real del archivo de base de datos
 
 # Ruta al archivo PDF
-# pdf_path = 'ruta/a/tu/documento.pdf'  # Cambia esto por la ruta real del archivo PDF
-pdf_path = r'E:\OneDrive\DATOS\99 - SETELECO\TAREAS SETELECO\04_Tarea_UNICODatos_NDA\Tarea_UNICODatos_NDA\ELANTA_Lyntia_Formulario_Registro_Operadora_UNICO_DATOS_v3 firmado 3.pdf'  # Cambia esto por la ruta real del archivo PDF
+pdf_path = 'ruta/a/tu/documento.pdf'  # Cambia esto por la ruta real del archivo PDF
 
 # Verificar que las rutas existan antes de proceder
 if not os.path.exists(db_path):
